b6fe93f8b7c1d233e756d816e25a662d71b22ff3
Hiter Harris <hiterharris@comcast.net>
2020-01-21 14:37:34 -0500

    random image rendered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const apiKey = "ac52df63d21b30457177b47056671a7f567ad59f2917eca8119d9b38f9b04387";
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://api.unsplash.com/photos/random?client_id=${apiKey}`)
+      .then(response => {
+        console.log(response.data);
+        setData(response.data.urls.regular);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={data} alt="Unsplash" />
     </div>
   );
 }
-
-export default App;
