905c2b7a09e8673f21743a544ec99810c689ed27
Henry H. Harris (Signature) <hharris@MacBook-Pro-2.local>
2020-10-01 08:30:46 -0400

    render specific image height

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import axios from "axios";
 import "./App.css";
 
 export default function App() {
-  const apiKey = "33678ea5d2162d78ed95b142d75c2bb6f0d98ddbe7ed96a949da41989f4269d6";
+  const apiKey = "Ce1NlWlzsDRByTbBB-TlkrVKHmW2PK1TD6xry1X4gK8";
   const [data, setData] = useState([]);
   useEffect(() => {
     axios
       .get(`https://api.unsplash.com/photos/random?client_id=${apiKey}`)
       .then(response => {
         console.log(response.data);
+        setData(response.data);
         setData(response.data.urls.regular);
+        if (response.data.height < 4000) {
+          return true;
+        } else {
+          return false;
+        }
       });
   }, []);
   return (
